9849deef3ebd6e5425e6643ac9330c048fc46efa
Lebid_Dmytro <lebedd99@gmail.com>
2025-12-11 15:45:04 +0200

    Add database models: User, UserProfile, UserGroup with enums

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User, UserProfile, UserGroup
+from app.models.enums import UserGroupEnum, GenderEnum
+
+__all__ = ["User", "UserProfile", "UserGroup", "UserGroupEnum", "GenderEnum"]
+
---
+++ app/models/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class UserGroupEnum(str, Enum):
+    USER = "USER"
+    MODERATOR = "MODERATOR"
+    ADMIN = "ADMIN"
+
+
+class GenderEnum(str, Enum):
+    MAN = "MAN"
+    WOMAN = "WOMAN"
+
